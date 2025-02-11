2fbb1a77fe5dbf55c61984800439f213f632e697
Shermy-netizon <shermy@blarve.com>
2025-02-11 18:03:08 +0530

    first commit

---
+++ resources/views/livewire/components/sidebar.blade.php
@@ -0,0 +1,61 @@
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
+    <div class="sidebar-brand"> <!--begin::Brand Link--> <a href="../index.html" class="brand-link">
+            <!--begin::Brand Image--> <img src="{{ asset('assets/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+                class="brand-image opacity-75 shadow"> <!--end::Brand Image--> <!--begin::Brand Text--> <span
+                class="brand-text fw-light">AdminLTE 4</span> <!--end::Brand Text--> </a> <!--end::Brand Link--> </div>
+    <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
+    <div class="sidebar-wrapper">
+        <nav class="mt-2"> <!--begin::Sidebar Menu-->
+            <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
+                <li class="nav-item"> <a href="#" class="nav-link"> <i class="nav-icon bi bi-speedometer"></i>
+                        <p>
+                            Products
+                            <i class="nav-arrow bi bi-chevron-right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item"> <a href="" class="nav-link"> <i
+                                    class="nav-icon bi bi-circle"></i>
+                                <p>Catogories</p>
+                            </a> </li>
+                        <li class="nav-item"> <a href="" class="nav-link"> <i
+                                    class="nav-icon bi bi-circle"></i>
+                                <p>ProductList</p>
+                            </a> </li>
+                        <li class="nav-item"> <a href="" class="nav-link"> <i
+                                    class="nav-icon bi bi-circle"></i>
+                                <p>Brand</p>
+                            </a> </li>
+                    </ul>
+                </li>
+                <li class="nav-item"> <a href="" class="nav-link"> <i
+                            class="nav-icon bi bi-palette"></i>
+                        <p>Tables</p>
+                    </a> </li>
+                <li class="nav-item"> <a href="" class="nav-link"> <i
+                            class="nav-icon bi bi-palette"></i>
+                        <p>Services</p>
+                    </a> </li>
+                <li class="nav-item"> <a href="" class="nav-link"> <i
+                            class="nav-icon bi bi-palette"></i>
+                        <p>Suppliers</p>
+                    </a> </li>
+                <li class="nav-item"> <a href="" class="nav-link"> <i
+                            class="nav-icon bi bi-palette"></i>
+                        <p>Division</p>
+                    </a> </li>
+                <li class="nav-item"> <a href="" class="nav-link"> <i
+                            class="nav-icon bi bi-palette"></i>
+                        <p>Classes</p>
+                    </a> </li>
+                <li class="nav-item"> <a href="" class="nav-link"> <i
+                            class="nav-icon bi bi-palette"></i>
+                        <p>Student</p>
+                    </a>
+                </li>
+            </ul>
+
+
+        </nav>
+    </div> <!--end::Sidebar Wrapper-->
+</aside> <!--end::Sidebar--> <!--begin::App Main-->
---
+++ routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/',\App\Livewire\HomePage::class)->name('home');
+
